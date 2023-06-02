f0179f22736a0ac40c1dbecf1291ebb9a1cc79ff
jkruszynski <jozef@krush.at>
2023-06-02 18:18:52 +0200

    MVP part 1

--- release-notes-merge-action.py
+++ release-notes-merge-action.py
@@ -1,45 +1,11 @@
 import argparse
 import datetime
 
-import markdown
 from github import Github
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    "--pre_release",
-    type=str,
-    help="Whether or not this is a pre-release.",
-    required=True,
-)
-parser.add_argument("--github_token", type=str, help="Github token.", required=True)
-parser.add_argument(
-    "--new_frontend_tag",
-    type=str,
-    help="The new frontend tag to use for the release.",
-    required=True,
-)
-parser.add_argument(
-    "--new_frontend_release_title",
-    type=str,
-    help="The new frontend release title to use for the release.",
-    required=True,
-)
-parser.add_argument(
-    "--new_server_tag",
-    type=str,
-    help="The new server tag to use for the release.",
-    required=True,
-)
-parser.add_argument(
-    "--new_server_release_title",
-    type=str,
-    help="The new server release title to use for the release.",
-    required=True,
-)
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
     args = parser.parse_args()
 
     ORGANIZATION = "music-assistant"
@@ -56,22 +22,32 @@ if __name__ == "__main__":
 
     frontend_release = frontend_repo.get_latest_release()
 
-    if args.pre_release == "true":
-        server_latest_release = next(
-            filter(lambda release: release.prerelease, server_repo.get_releases())
-        )
-    else:
-        server_latest_release = server_repo.get_latest_release()
+    server_latest_release = next(
+        filter(lambda release: release.prerelease, server_repo.get_releases())
+    )
 
-    foobar = markdown.markdown(server_latest_release.body)
+    foobar = server_latest_release.body
 
     changelog_file = target_repo.get_contents("CHANGELOG.md", ref="main")
-    foo = markdown.markdown(changelog_file.decoded_content.decode("utf-8"))
+    foo = changelog_file.decoded_content.decode("utf-8")
     log_date = datetime.datetime.now().strftime("%m-%d-%Y")
 
-    updated_changelog = markdown.markdown(
-        f"# {server_latest_release.tag_name} - [{log_date}]\n# Frontend\n{frontend_release.body}\n# Server\n{server_latest_release.body}\n{foo}"
-    )
+    updated_changelog = """
+    # {server_latest_release.tag_name} - [{log_date}]
+    
+    # Frontend
+    
+    {frontend_release.body}
+    
+    # Server
+    
+    {server_latest_release.body}
+    
+    # Test
+    
+    {foo}
+    
+    """
 
     # target_repo.update_file("CHANGELOG.md", f"Update CHANGELOG.md for {server_latest_pre_release.tag_name}", updated_changelog, changelog_file.sha, branch="main")
 
