b19726415fa8e92c8122fe4d485a48e65cf6f865
jkruszynski <jozef@krush.at>
2023-06-04 15:51:31 +0200

    Test

--- release-notes-merge-action.py
+++ release-notes-merge-action.py
@@ -4,7 +4,38 @@ import datetime
 from github import Github
 
 parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--pre_release",
+    type=str,
+    help="Whether or not this is a pre-release.",
+    required=True,
+)
 parser.add_argument("--github_token", type=str, help="Github token.", required=True)
+parser.add_argument(
+    "--new_frontend_tag",
+    type=str,
+    help="The new frontend tag to use for the release.",
+    required=True,
+)
+parser.add_argument(
+    "--new_frontend_release_title",
+    type=str,
+    help="The new frontend release title to use for the release.",
+    required=True,
+)
+parser.add_argument(
+    "--new_server_tag",
+    type=str,
+    help="The new server tag to use for the release.",
+    required=True,
+)
+parser.add_argument(
+    "--new_server_release_title",
+    type=str,
+    help="The new server release title to use for the release.",
+    required=True,
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -23,38 +54,42 @@ if __name__ == "__main__":
 
     frontend_release = frontend_repo.get_latest_release()
 
-    server_latest_release = next(
-        filter(lambda release: release.prerelease, server_repo.get_releases())
-    )
+    if args.pre_release == "true":
+        server_latest_release = next(
+            filter(lambda release: release.prerelease, server_repo.get_releases())
+        )
+    else:
+        server_latest_release = server_repo.get_latest_release()
 
-    foobar = server_latest_release.body
+    if args.new_frontend_tag != frontend_release.tag_name:
+        raise ValueError("Frontend tag does not match the latest release tag.")
 
-    changelog_file = target_repo.get_contents("CHANGELOG.md", ref="main")
-    foo = changelog_file.decoded_content.decode("utf-8")
-    log_date = datetime.datetime.now().strftime("%m-%d-%Y")
-
-    updated_changelog = f"""
-    # {server_latest_release.tag_name} - [{log_date}]
-    
-    # Frontend
-    
-    {frontend_release.body}
-    
-    # Server
-    
-    {server_latest_release.body}
-    
-    # Test
-    
-    {foo}
-    
-    """
-
-    # target_repo.update_file("CHANGELOG.md", f"Update CHANGELOG.md for {server_latest_pre_release.tag_name}", updated_changelog, changelog_file.sha, branch="main")
-
-    print(updated_changelog)
+    if args.new_server_tag != server_latest_release.tag_name:
+        raise ValueError("Server tag does not match the latest release tag.")
 
     target_release = target_repo.get_latest_release()
     target_release_id = target_release.id
 
-    # target_release.update_release(name="The first test", message=f"{REPO_1}{release1.body}\r\n{REPO_2}{release2.body}")
+    changelog_file = target_repo.get_contents("CHANGELOG.md", ref="main")
+    existing_changelog_content = changelog_file.decoded_content.decode("utf-8")
+    log_date = datetime.datetime.now().strftime("%d.%m.%Y")
+
+    updated_changelog = f"# {server_latest_release.tag_name} - [{log_date}]\n\n"
+    updated_changelog += "# Frontend\n\n"
+    updated_changelog += f"{frontend_release.body}\n\n"
+    updated_changelog += "# Server\n\n"
+    updated_changelog += f"{server_latest_release.body}\n\n"
+
+    target_release.update_release(name="The first test", message=updated_changelog)
+
+    updated_changelog += f"{existing_changelog_content}\n\n"
+
+    target_repo.update_file(
+        "CHANGELOG.md",
+        f"Update CHANGELOG.md for {server_latest_release.tag_name}",
+        updated_changelog,
+        changelog_file.sha,
+        branch="main",
+    )
+
+    print(updated_changelog)
